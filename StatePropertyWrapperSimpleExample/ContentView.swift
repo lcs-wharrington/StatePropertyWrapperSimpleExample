fedb96932f69f41c92eb6f9ddba11a1cd7b6c79a
William Harrington <wharrington@lcs.on.ca>
2021-11-29 11:23:45 -0500

    Initial Commit

---
+++ StatePropertyWrapperSimpleExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StatePropertyWrapperSimpleExample
+//
+//  Created by William Robert Harrington on 2021-11-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StatePropertyWrapperSimpleExample/StatePropertyWrapperSimpleExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatePropertyWrapperSimpleExampleApp.swift
+//  StatePropertyWrapperSimpleExample
+//
+//  Created by William Robert Harrington on 2021-11-29.
+//
+
+import SwiftUI
+
+@main
+struct StatePropertyWrapperSimpleExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
